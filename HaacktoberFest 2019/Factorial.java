3454b59a77b3ab610ed2f703a899fa8b3993ef0b
AhomBoy <madangogoi452@gmail.com>
2019-10-20 21:05:02 +0530

    Update Factorial.java

--- HaacktoberFest 2019/Factorial.java
+++ HaacktoberFest 2019/Factorial.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.BufferedReader;
  
 class Factorial
 {
@@ -7,17 +7,18 @@ class Factorial
       int a;
     int  c;
     int fact = 1;
+    BufferedReader o1 =  new BufferedReader(inputStreamReader(system.in));
+    
      
       System.out.println(" Enter an integer ");
-      Scanner sc = new Scanner(System.in);
-     
-      a = sc.nextInt();
+      int num = o1.readLine();
+     a = Integer.parseInt ( num );
      
       if (a < 0)
          System.out.println("Number should be non-negative.");
       else
       {
-         for (c = 1; c <= a; c++)
+         for (c = a; c >= 0; c--)
             fact = fact*c;
      
          System.out.println("Factorial of "+a+" is = "+fact);
